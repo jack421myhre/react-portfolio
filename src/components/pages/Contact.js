7338c007399411c370b5e4316cb3e7a785086f5a
Jackson Myhre <jack421myhre@gmail.com>
2022-11-02 23:49:02 -0500

    Removed code causing deployment issues

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../styles/Contact.css';
-import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
 
-    const [formContent, setFormContent] = useState({ name: '', email: '', message: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const handleInputChange = (e) => {
-        const inputType = e.target.name;
-        const inputValue = e.target.value;
-
-        if (!inputValue) {
-            setErrorMessage(`Make sure your ${inputType} is filled out before submitting.`);
-        } else if (inputType === 'email') {
-            const validEmail = validateEmail(inputValue);
-            if (!validEmail) {
-                setErrorMessage('Please enter a valid email address.');
-            } return;
-        } else if (!errorMessage) {
-            setFormContent({ ...setFormContent, [inputType]: inputValue });
-        }
-    };
 
     return (
         <div className='d-flex flex-column align-items-center contact-container'>
@@ -33,11 +14,11 @@ function Contact() {
                 <form>
                     <div className='mb-3'>
                         <label for="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" onChange={handleInputChange} />
+                        <input type="text" className="form-control" />
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" aria-describedby="emailHelp" onChange={handleInputChange} />
+                        <input type="email" className="form-control" aria-describedby="emailHelp" />
                         <div className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
