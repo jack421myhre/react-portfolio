6640ebd79fc9abd7357611f74b603471297e597b
Jackson Myhre <jack421myhre@gmail.com>
2022-11-02 21:31:17 -0500

    Added headshot image, changed homepage to be the about page, removed icons and added text for the about me

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -3,53 +3,55 @@ import '../styles/Nav.css';
 
 function NavLinks({ currentPage, handlePageChange }) {
     return (
-        <ul className="nav">
-            <li className="nav-item">
+        <div className='d-flex flex-row justify-content-between align-items-center nav-container'>
+            <div>
                 <a
                     href="#home"
                     onClick={() => handlePageChange('Home')}
                     className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
                 >
-                    Home
+                    <p className='nav-name'>JM</p>
                 </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#about"
-                    onClick={() => handlePageChange('About')}
-                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#work"
-                    onClick={() => handlePageChange('Work')}
-                    className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
-                >
-                    My Work
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#resume"
-                    onClick={() => handlePageChange('Resume')}
-                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                    Resume
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#contact"
-                    onClick={() => handlePageChange('Contact')}
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact Me
-                </a>
-            </li>
-        </ul>
+            </div>
+            <ul className="nav">
+                <li className="nav-item">
+                    <a
+                        href="#about"
+                        onClick={() => handlePageChange('About')}
+                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                    >
+                        About
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#work"
+                        onClick={() => handlePageChange('Work')}
+                        className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+                    >
+                        My Work
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#resume"
+                        onClick={() => handlePageChange('Resume')}
+                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Resume
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="#contact"
+                        onClick={() => handlePageChange('Contact')}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Contact Me
+                    </a>
+                </li>
+            </ul>
+        </div>
     );
 }
 
